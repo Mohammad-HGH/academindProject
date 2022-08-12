0247613babdba144a44813e3ff282aed0f120611
mohammad-hgh <88213502+Mohammad-HGH@users.noreply.github.com>
2022-08-12 12:17:39 +0430

    new push with force flag

---
+++ meetups/views.py
@@ -0,0 +1,13 @@
+# * what should happen when certain url entered.
+
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+
+def index():
+    # tell to browser what send to user screen.
+    # Django will call it when we're having incoming req for a certain url.
+    return
